ae48c5cf1bb0f20287fc4f83464664533e590a94
nosi <shtldudv@naver.com~>
2024-10-22 19:20:04 +0900

    TEST

--- JavaStudy/src/main/java/synoh/test.java
+++ JavaStudy/src/main/java/synoh/test.java
@@ -3,7 +3,7 @@ package synoh;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("nosi");
-		System.out.println("������ �����Ϸ��� �մϴ�. ���� ��Ź�帳�ϴ�.");
+		System.out.println("자바를 독학으로 공부하여 개발자로 취직할 예정입니다..");
 	}
 }
 

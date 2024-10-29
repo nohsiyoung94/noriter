bbf63a121ca1e8f6c55d7df6d9bcb19588522ff7
uno-km <zhfldk014745@naver.com>
2024-10-29 23:43:23 +0900

    👓 깊게 공부

--- PythonStudy/Uno/Crawling/BeautifulSoup/BeautifulSoup_2.py
+++ PythonStudy/Uno/Crawling/BeautifulSoup/BeautifulSoup_2.py
@@ -1,10 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-
 html = urlopen('https://finance.naver.com/')
 bs = BeautifulSoup(html, 'html.parser')
-
-nameList = bs.findAll('span',{'class':'txt'})
-print(nameList)
+nameList = bs.findAll('img', limit=5)
 for name in nameList:
-    print(name.get_text())
+    print(name)
